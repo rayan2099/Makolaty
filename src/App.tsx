236353b0a567cd65c3ffa7cb24a35bdf53088825
rayan2099 <rayansabih@protonmail.com>
2026-07-15 16:53:12 +0300

    Enlarge drink can and juice images

--- src/App.tsx
+++ src/App.tsx
@@ -280,7 +280,7 @@ const MenuItemImage = ({ item }: { item: MenuItem }) => {
           : shouldUseSquareFrame
             ? "object-cover"
             : shouldUseDrinkFrame
-              ? "object-contain p-2"
+              ? "object-contain p-1"
               : "object-cover group-hover:scale-110"
       )}
       referrerPolicy="no-referrer"
